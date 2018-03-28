8570f68da3db703d53c92fccd1e53bb61b5fa446
Jose Ignacio Zuñiga <jose.zuniga@direcciondecompras.local>
2018-03-28 18:00:46 -0300

    imagen logo por svg color nav

--- client/src/components/shared/Nav.js
+++ client/src/components/shared/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from '../../assets/img/LogoMP2.png'
+import logo from '../../assets/img/logoChc_blanco.svg'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Buscador from '../Buscador'
 
 const Nav = () => (
-  <nav className="navbar navbar-light bg-light justify-content-between py-4">
-    <Link to ='/' className="navbar-brand"><img src={logo} alt="" style={{ 'height': '44px' }} /></Link>
+  <nav className="navbar navbar-light bg-dark justify-content-between py-4">
+    <Link to ='/' className="navbar-brand pt-0"><img src={logo} alt="" style={{ 'height': '50px' }} /></Link>
     <Buscador />
   </nav>
 )
